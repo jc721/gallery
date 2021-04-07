57cbb08eea17061fcc0f8d59a280db6fc6ad5f4d
ArthurNNN <nikitinarthur@gmail.com>
2021-04-07 19:23:21 +0200

    gallery v1.0

--- app.js
+++ app.js
@@ -12,4 +12,28 @@ fetch('url').then(ResolveFuntion, RejectFuntion)
 
 */
 
+fetch(URL)
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    let divR = document.createElement("div");
+    divR.className = "row";
+    app.appendChild(divR);
+    data.forEach(item => {
+      let divC = document.createElement("div");
+      divC.className = "column";
+      let img = document.createElement("img");
+      img.src = item.url;
+      divC.appendChild(img);
+      divR.appendChild(divC);
+    });
+  });
+
 const app = document.querySelector("#app");
+
+
+
+
